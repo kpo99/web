7f32aedcbbad58702853e96be45c24d596005cd9
kpo99 <krivenkopetr99@gmail.com>
2016-12-21 06:52:18 +0200

    test

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ require('./config/passport')(app);
 
 /* Routers */
 app.use(function(req,res,next){
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://glacial-castle-57624.herokuapp.com');
 
 // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
